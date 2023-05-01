760f8641b6a8dcd818c4cbc881209484621dc36f
mumo-esther <mumoesther2021@gmail.com>
2023-05-01 16:57:51 +0300

    Add SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3

--- queries.sql
+++ queries.sql
@@ -6,3 +6,5 @@ SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
