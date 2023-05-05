a9a3f9c37a09a40bef18158d469374f90725c6c8
mumo-esther <mumoesther2021@gmail.com>
2023-05-05 09:48:54 +0300

    populated queries.sql

--- data.sql
+++ data.sql
@@ -56,91 +56,6 @@ VALUES
 ('Blossom', '1998-10-13', 3, true, 17, ''),
 ('Ditto', '2022-05-14', 4, true, 22, '');
 
-SELECT * FROM animals;
-
-
-START TRANSACTION;
-SET SQL_SAFE_UPDATES = 0;
-UPDATE animals SET species = 'unspecified';
-SELECT * FROM animals;  -- verify that the change was made
-ROLLBACK;
-SELECT * FROM animals;  -- verify that the change was rolled back
-SET SQL_SAFE_UPDATES = 0;
-SELECT * FROM animals;
-
-START TRANSACTION;
-UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-UPDATE animals SET species = 'pokemon' WHERE species = '';
-COMMIT;
-SELECT * FROM animals;
-SELECT * FROM animals WHERE species = 'pokemon';
-
-START TRANSACTION;
-DELETE FROM animals;
-ROLLBACK;
-SELECT * FROM animals;
-
-START TRANSACTION;
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-SAVEPOINT sp;
-UPDATE animals SET weight_kg = -1 * weight_kg;
-ROLLBACK TO sp;
-UPDATE animals SET weight_kg = -1 * weight_kg WHERE weight_kg < 0;
-COMMIT;
-SELECT * FROM animals;
-
-SELECT COUNT(*) FROM animals;
-
-SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
-
-SELECT AVG(weight_kg) FROM animals;
-
-SELECT neutered, AVG(escape_attempts) as avg_escape_attempts
-FROM animals
-GROUP BY neutered
-ORDER BY avg_escape_attempts DESC
-LIMIT 1;
-
-SELECT species, MIN(weight_kg) as min_weight, MAX(weight_kg) as max_weight
-FROM animals
-GROUP BY species;
-
-SELECT species, AVG(escape_attempts) as avg_escape_attempts
-FROM animals
-WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
-GROUP BY species;
-
-
-SELECT * FROM animals;
-
-CREATE TABLE owners (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    full_name VARCHAR(100),
-    age INTEGER
-);
-CREATE TABLE species (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50)
-);
-
-ALTER TABLE animals
-    DROP COLUMN species,
-    ADD COLUMN species_id INTEGER,
-    ADD COLUMN owner_id INTEGER,
-    MODIFY COLUMN id INTEGER NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE animals
-    ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id)
-        REFERENCES species(id),
-    ADD CONSTRAINT fk_owner_id FOREIGN KEY (owner_id)
-        REFERENCES owners(id);
-
-INSERT INTO owners (full_name, age)
-VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
-
-INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
--- find owner_ids
-SELECT id, full_name FROM owners;
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species_id, owner_id)
 VALUES 
 ('Agumon', '2020-02-03', 0, false, 10.23, 2, 1),
@@ -154,6 +69,3 @@ VALUES
 ('Boarmon', '2005-06-07', 7, true, 20.4, 2, 5),
 ('Blossom', '1998-10-13', 3, true, 17, 1, 4),
 ('Ditto', '2022-05-14', 4, true, 22, 1, null);
-
-SELECT * FROM OWNERS;
-
--- queries.sql
+++ queries.sql
@@ -94,3 +94,88 @@ JOIN animals ON owners.id = animals.owner_id
 GROUP BY owners.full_name
 ORDER BY count DESC
 LIMIT 1;
+
+START TRANSACTION;
+SET SQL_SAFE_UPDATES = 0;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;  -- verify that the change was made
+ROLLBACK;
+SELECT * FROM animals;  -- verify that the change was rolled back
+SET SQL_SAFE_UPDATES = 0;
+SELECT * FROM animals;
+
+START TRANSACTION;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species = '';
+COMMIT;
+SELECT * FROM animals;
+SELECT * FROM animals WHERE species = 'pokemon';
+
+START TRANSACTION;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+START TRANSACTION;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT sp;
+UPDATE animals SET weight_kg = -1 * weight_kg;
+ROLLBACK TO sp;
+UPDATE animals SET weight_kg = -1 * weight_kg WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, AVG(escape_attempts) as avg_escape_attempts
+FROM animals
+GROUP BY neutered
+ORDER BY avg_escape_attempts DESC
+LIMIT 1;
+
+SELECT species, MIN(weight_kg) as min_weight, MAX(weight_kg) as max_weight
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) as avg_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
+
+
+SELECT * FROM animals;
+
+CREATE TABLE owners (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100),
+    age INTEGER
+);
+CREATE TABLE species (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+ALTER TABLE animals
+    DROP COLUMN species,
+    ADD COLUMN species_id INTEGER,
+    ADD COLUMN owner_id INTEGER,
+    MODIFY COLUMN id INTEGER NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE animals
+    ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id)
+        REFERENCES species(id),
+    ADD CONSTRAINT fk_owner_id FOREIGN KEY (owner_id)
+        REFERENCES owners(id);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
+-- find owner_ids
+SELECT id, full_name FROM owners;
+SELECT * FROM OWNERS;
+
